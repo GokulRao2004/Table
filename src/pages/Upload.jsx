ee3c6b3c2329a609d770833fb984fa1e7947cfe9
gokul-the-developer <62870769+gokul-the-developer@users.noreply.github.com>
2024-01-31 10:33:07 +0530

    Upload section only allows .xlsx files

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useRef } from 'react'
-import './upload.css'
-import axios from 'axios'
+import React, { useState, useRef } from 'react';
+import './upload.css';
+import axios from 'axios';
 import { getImageUrl } from '../utils';
 
 export const Upload = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
   var fileInputRef = useRef(null);
 
   const handleDragEnter = (e) => {
@@ -23,41 +25,64 @@ export const Upload = () => {
     setIsDragging(false);
 
     const file = e.dataTransfer.files[0];
-    setSelectedFile(file);
+    handleFileSelection(file);
   };
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
-    setSelectedFile(file);
+    handleFileSelection(file);
   };
+
+  const handleFileSelection = (file) => {
+    if (file && file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+      setSelectedFile(file);
+      setErrorMessage(null);
+    } else {
+      setSelectedFile(null);
+      setErrorMessage('Please select any other file. Only .xlsx files are supported');
+    }
+  };
+
   const handleUpload = () => {
     if (selectedFile) {
       const formData = new FormData();
       formData.append('file', selectedFile);
-
+  
       axios
-        .post('http://localhost:8085/process_excel', formData, {
+        .post('https://webhook.site/ba16d6fe-e366-4f56-a7ad-f73d5d776631', formData, {
           headers: {
-            'Content-Type': 'multipart/form-data',
+            'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
           },
         })
         .then((response) => {
-          console.log("This is success msg")
-          console.log(response.data);
+          console.log('This is success msg');
+          console.log(response.status);
+          
+          if (response.status === 200) {
+            setUploadSuccess(true);
+            setErrorMessage(`File ${selectedFile.name} uploaded successfully`)
+          } 
+          console.log(uploadSuccess);
+          
           // Handle success
         })
         .catch((error) => {
-          console.log("This is failure")
           console.error('Error uploading file', error);
+          setErrorMessage(`Error uploading file. Please try again.`);
+          setUploadSuccess(false);
           // Handle error
         });
-
-
-
-
     } else {
       console.warn('No file selected for upload');
+      setErrorMessage('No file selected for upload')
     }
+  };
+  
+
+  const handleReset = () => {
+    setUploadSuccess(false);
+    setSelectedFile(null);
+    setErrorMessage(null);
   };
 
   return (
@@ -71,18 +96,25 @@ export const Upload = () => {
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
       >
-        
-        <img src={getImageUrl('cloud-computing.png')} className='uploadImg'></img>
+        <img src={getImageUrl('cloud-computing.png')} className='uploadImg' alt='cloud-icon' />
         <input
           type="file"
           onChange={handleFileChange}
           style={{ display: 'none' }}
           ref={(fileInput) => (fileInputRef = fileInput)}
         />
-        <p className='uploadText'>{selectedFile ? `Selected File: ${selectedFile.name}` : 'Only .xlsx files are supported'}</p>
+        <p className={`uploadText ${errorMessage ? 'error' : ''}`}>
+          {errorMessage ? errorMessage : uploadSuccess ? 'File uploaded successfully!' : selectedFile ? `Selected File: ${selectedFile.name}` : 'Only .xlsx files are supported'}
+        </p>
 
-        <button onClick={() => fileInputRef.click()}>Select File</button>
-        <button onClick={handleUpload}>Upload</button>
+        {!uploadSuccess && (
+          <>
+            <button onClick={() => fileInputRef.click()}>Select File</button>
+            <button onClick={handleUpload}>Upload</button>
+          </>
+        )}
+
+        {uploadSuccess && <button onClick={handleReset}>Upload Another File</button>}
       </div>
     </div>
   );
